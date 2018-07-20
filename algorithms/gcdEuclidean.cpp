5ba00bd832ef260a4cea8a070e627425fb519bac
AlekseiMaide <aleksei.maide@gmail.com>
2018-07-21 00:47:32 +0300

    Euclidean GCD note added.

--- algorithms/gcdEuclidean.cpp
+++ algorithms/gcdEuclidean.cpp
@@ -1,5 +1,7 @@
 /**
     O(log n) Euclidean algorithm for finding greatest common divisor.
+
+    By adding a check for whether one value is bigger than the second on, one can improve the performance in half of the cases.
 */
 
 int gcdEuclidean(int a, int b) {
