67175e7c89aa70412b8bb907e9dda588d15d4248
AlekseiMaide <aleksei.maide@gmail.com>
2018-08-28 22:39:29 +0300

    Sum of geometric series added.

--- algorithms/seriesAndSequences.cpp
+++ algorithms/seriesAndSequences.cpp
@@ -1,5 +1,9 @@
 //Todo: Add arichmetic, geometric, harmonic series.
 
+int sumOfGeometricSeries(int n) {
+    return std::pow(2, n) - 1;
+}
+
 //Calculate the sum of squares from 1..n
 int sumOfSquares(int n) {
     return (n * (n + 1) * (2 * n + 1)) / 6;
