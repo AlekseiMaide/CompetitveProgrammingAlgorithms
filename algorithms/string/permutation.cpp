6cfdeba7117edc31999a389f9a362d64e3ec462c
AlekseiMaide <aleksei.maide@gmail.com>
2021-06-03 21:32:42 +0200

    heap's algorithm for generating permutations

--- algorithms/string/permutation.cpp
+++ algorithms/string/permutation.cpp
@@ -29,3 +29,25 @@ bool nextPermutation(string &s) {
 
     return false;
 }
+
+/**
+    Heap's algorithm for generating permutations.
+**/
+void heapPermutation(string a, int size) {
+    if (size == 1) {
+        cout << a << endl;
+        return;
+    }
+
+    for (int i = 0; i < size; i++) {
+        heapPermutation(a, size - 1);
+
+        if (size % 2 == 1) {
+            // if size is odd, swap 0th and (size-1)th element
+            swap(a[0], a[size - 1]);
+        } else {
+            // If size is even, swap ith and (size-1)th element
+            swap(a[i], a[size - 1]);
+        }
+    }
+}
