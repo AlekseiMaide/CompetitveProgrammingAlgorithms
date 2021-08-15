0c17000f8e8504fc61feb613d25bb151ba1352c1
AlekseiMaide <aleksei.maide@gmail.com>
2021-08-15 13:37:13 +0200

    add convex hull

---
+++ algorithms/geometry/graham_scan.cpp
@@ -0,0 +1,137 @@
+/* 
+ * Reference: https://www.nayuki.io/page/convex-hull-algorithm
+ *
+ *
+ * Convex hull algorithm - Library (C++)
+ * 
+ * Copyright (c) 2021 Project Nayuki
+ * https://www.nayuki.io/page/convex-hull-algorithm
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program (see COPYING.txt and COPYING.LESSER.txt).
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <vector>
+#include <algorithm>
+
+
+struct Point final {
+	
+	public: double x;
+	public: double y;
+	
+	public: bool operator==(const Point &other) const;
+	public: bool operator!=(const Point &other) const;
+	public: bool operator< (const Point &other) const;
+	public: bool operator<=(const Point &other) const;
+	public: bool operator> (const Point &other) const;
+	public: bool operator>=(const Point &other) const;
+	
+};
+
+
+// Returns a new list of points representing the convex hull of
+// the given set of points. The convex hull excludes collinear points.
+// This algorithm runs in O(n log n) time.
+vector<Point> makeConvexHull(std::vector<Point> points);
+
+// Returns the convex hull, assuming that each points[i] <= points[i + 1]. Runs in O(n) time.
+vector<Point> makeConvexHullPresorted(const std::vector<Point> &points);
+
+
+bool Point::operator==(const Point &other) const {
+	return x == other.x && y == other.y;
+}
+
+bool Point::operator!=(const Point &other) const {
+	return x != other.x || y != other.y;
+}
+
+bool Point::operator<(const Point &other) const {
+	if (x != other.x) return x < other.x;
+	else return y < other.y;
+}
+
+bool Point::operator>(const Point &other) const {
+	if (x != other.x) return x > other.x;
+	else return y > other.y;
+}
+
+bool Point::operator<=(const Point &other) const {
+	if (x != other.x) return x < other.x;
+	else return y <= other.y;
+}
+
+bool Point::operator>=(const Point &other) const {
+	if (x != other.x) return x > other.x;
+	else return y >= other.y;
+}
+
+
+vector<Point> makeConvexHull(vector<Point> points) {
+	std::sort(points.begin(), points.end());
+	return makeConvexHullPresorted(points);
+}
+
+
+vector<Point> makeConvexHullPresorted(const vector<Point> &points) {
+	if (points.size() <= 1)
+		return vector<Point>(points);
+	
+	// Andrew's monotone chain algorithm. Positive y coordinates correspond to "up"
+	// as per the mathematical convention, instead of "down" as per the computer
+	// graphics convention. This doesn't affect the correctness of the result.
+	
+	vector<Point> upperHull;
+	for (const Point &p : points) {
+		while (upperHull.size() >= 2) {
+			const Point &q = *(upperHull.cend() - 1);  // Same as .back()
+			const Point &r = *(upperHull.cend() - 2);
+			if ((q.x - r.x) * (p.y - r.y) >= (q.y - r.y) * (p.x - r.x)) {
+                upperHull.pop_back();
+            } else {
+                break;
+            }
+		}
+		upperHull.push_back(p);
+	}
+	upperHull.pop_back();
+	
+	vector<Point> lowerHull;
+	for (vector<Point>::const_reverse_iterator it = points.crbegin(); it != points.crend(); ++it) {
+		const Point &p = *it;
+		while (lowerHull.size() >= 2) {
+			const Point &q = *(lowerHull.cend() - 1);  // Same as .back()
+			const Point &r = *(lowerHull.cend() - 2);
+			if ((q.x - r.x) * (p.y - r.y) >= (q.y - r.y) * (p.x - r.x)) {
+                lowerHull.pop_back();
+            } else {
+                break;
+            }
+		}
+
+		lowerHull.push_back(p);
+	}
+	lowerHull.pop_back();
+	
+	if (!(upperHull.size() == 1 && upperHull == lowerHull))
+		upperHull.insert(upperHull.end(), lowerHull.cbegin(), lowerHull.cend());
+
+	return upperHull;
+}
+
+// Todo: Implement function to check if within bounds of a convex hull.
+//      Check if a point is to the left of the generated convex hull.
--- algorithms/math/seriesAndSequences.cpp
+++ algorithms/math/seriesAndSequences.cpp
@@ -1,7 +1,11 @@
 //Todo: Add arichmetic, geometric, harmonic series.
 
 int sumOfGeometricProgression(int firstTerm, int ratio, int n) {
-    return (firstTerm * (1 – pow(ratio, n))) / (1 – ratio);
+    return (firstTerm * (1 - pow(ratio, n))) / (1 - ratio);
+}
+
+double sumOfGeometricProgression(double firstTerm, double ratio) {
+    return firstTerm / (1 - ratio);
 }
 
 //Calculate the sum of squares from 1..n
